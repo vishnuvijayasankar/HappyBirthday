7cec89ccee04d9ca4299d4628b922afdfcccb4f0
Vishnu Vijayasankar <vishnuvijayasankar@gmail.com>
2017-10-05 21:29:41 +0530

    Video and Audio added

--- birthday.js
+++ birthday.js
@@ -1,59 +1,30 @@
 
     var i;
-    var audio = new Audio('audio/HappyBirthdaySong.mp3');
-    // var arr = [
-    //     {
-    //         "url" : "videos/SampleVideo1.mp4"
-    //     }
-    // ];
+    // document.addEventListener('contextmenu', event => event.preventDefault());
+    // $(document).keydown(function(e){
+    //     e.preventDefault();
+    // });
+    var audio = new Audio('audio/FinalSong.mp3');
 
     function open_curtain() {
         $("#curtain1").animate({width:20},1000);
         $("#curtain2").animate({width:20},1000);
         $('.clickBtn').fadeOut( 600 );
         audio.play();
-
+        setTimeout(function(){ 
+            $(".gift").fadeIn(3000);
+        }, 6000);
     }
-
-    // function loadVideo() {
-    //     for(i=0; i<arr.length; i++) {
-    //         if(i == 0) {
-    //             var video = $('<video />', {
-    //             id: i,
-    //             src: arr[i].url,
-    //             type: 'video/mp4',
-    //             controls: false
-    //         });   
-    //             video.appendTo($('.mainVideo'));
-    //             video[0].autoplay = true;
-    //             video[0].load();
-    //             video[0].className = "video"+i;
-    //         }
-    //         else
-    //         doSetTimeout(i);
-    //     }
-
-    //     function doSetTimeout(i) {
-    //         setTimeout(function() {
-    //             debugger 
-    //         var video = $('<video />', {
-    //             id: i,
-    //             src: arr[i].url,
-    //             type: 'video/mp4',
-    //             controls: false
-    //         });   
-    //             video.appendTo($('.mainVideo'));
-    //             video[0].autoplay = true;
-    //             video[0].load();
-    //             video[0].className = "video"+i;
-    //          }, 5000);
-    //     }
-    // }
 
 function openModal(){
     audio.pause();
     audio.currentTime = 0;
-    debugger
     var vid = document.getElementById("myVideo");
     vid.play();
+}
+
+function closeModal(){
+    var vid = document.getElementById("myVideo");
+    vid.pause();
+    vid.currentTime = 0;
 }
